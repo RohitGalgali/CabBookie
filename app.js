48e06ed49492b57fd57b511768030f240a4892cb
Rohit <rohit.galgali@gmail.com>
2018-06-18 18:17:53 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+var path = require('path');
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res){
+    res.render("Home");
+});
+
+app.get('/availablecars/:id', function(req,res){
+    if(req.params.id == 'a2p')
+    {
+        res.render("availableCars");
+    }
+    else if(req.params.id == 'p2a')
+    {
+        res.render("AvailableCars");
+    }
+    // res.sendFile(path.join(__dirname, 'View', 'AvailableCars.html'));
+    console.log(req.params.id);
+})
+
+
+app.use(express.static('Resources'));
+
+app.listen(777);
