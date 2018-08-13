7214d9bb773650b3ef067b8c9d0240f50ae814d2
Rohit <rohit.galgali@gmail.com>
2018-08-13 16:18:57 +0530

    PayU money request JS

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var sha512 = require("js-sha512");
 const carModel = require("./Model/CarModel");
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: true });
+var request = require('request');
 app.set('view engine', 'ejs');
+
+var header ='fblhmYVnPpKm+Ng7Nz30WpckWGGmVLZDe/j6sNawbbo='; //will be provided by payumoney
 
 app.get('/', function(req,res){
     res.render("Home");
@@ -50,18 +53,44 @@ app.post('/payMoney', urlencodedParser, function (req, res) {
     hashString += "BJZPBL6X|";
     hashString += req.body.txnid + "|";
     hashString += req.body.amount + "|";
+    hashString += req.body.productinfo + "|";
     hashString += req.body.firstname + "|";
     hashString += req.body.email + "|";
     hashString += "||||||||||"
     hashString += salt;
+
     console.log(hashString);
-    //console.log(req.body);
-    // var salt = 'DiaeHToBX8';
+
     var hash = sha512(hashString);
     console.log(hash);
-    // res.send({success : true}); 
-    // , hash: hash
-    res.send(req.body);
+   
+    //res.send(req.body);
+    var payuData = {
+        key: 'BJZPBL6X',
+        salt: salt,
+        service_provider: 'payu_paisa',
+        hash: hash
+    };
+
+    var paymentData = {
+        productinfo: req.body.productinfo,
+        txnid: req.body.txnid,
+        amount: req.body.amount,
+        email: req.body.email,
+        phone: req.body.phone,
+        lastname: req.body.lastname,
+        firstname: req.body.firstname,
+        surl: "http://localhost:777/success", //"http://localhost:3000/payu/success"
+        furl: "http://localhost:777/success", //"http://localhost:3000/payu/fail"
+    };
+
+    var params = Object.assign(payuData, paymentData);
+    
+    request.post("https://sandboxsecure.payu.in/_payment", { form: params, headers: header }, function(error, response) {
+        if (!error) {
+            res.redirect(response.headers.location);
+        }
+    });
 });
 
 app.use(express.static('Resources'));
